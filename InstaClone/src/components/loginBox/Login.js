8807dd3c21a9c43fbdbcb45eba4467c5b118a5a5
quinn <williammsullivansabre@gmail.com>
2024-02-28 12:20:58 +0000

    enhancement: components

---
+++ InstaClone/src/components/loginBox/Login.js
@@ -0,0 +1,15 @@
+import "./login.css";
+import { useState } from "react";
+import { login } from "../../utils/fetch";
+
+const login = () => {
+  return (
+    <>
+      <div className="wrapperLogin">
+        <div className="loginBox">
+          <h2 className="loginTitle">Clonestagram</h2>
+        </div>
+      </div>
+    </>
+  );
+};
